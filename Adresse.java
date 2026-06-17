907929f3364e10830a959bdcc00c1d9abfd005c8
tbouzid56 <tawfikbouzid56@gmail.com>
2026-06-17 14:37:12 +0200

    new file:   Adresse.java

---
+++ Adresse.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+Adresse {
+    private String numero;
+    private String rue;
+    private String ville;
+    private String codePostale;
+
+    public String getNumero(){
+        return numero;
+    }
+
+    public void setNumero(String n){
+        this.numero = n;
+    }
+
+    public String getRue(){
+        return rue;
+    }
+
+    public void setRue(String r){
+        this.rue = r;
+    }
+
+    public String getVille(){
+        return ville;
+    }
+
+    public void setVille(String v){
+        this.ville = v
+    }
+
+    public String getCodePostale(){
+        return codePostale;
+    }
+
+    public void setCodePostale(String cP){
+        this.codePostale = cP;
+    }
+}
